60c4d7de3b7b1c061fe00b514ebb203baf8cb7e9
Yug Gurnani <yug.gurnani@codingninjas.com>
2024-03-02 14:58:20 +0530

    Added functionality for moving spacecraft forward without changing directions

--- chandrayaan.rb
+++ chandrayaan.rb
@@ -1,2 +1,15 @@
 class Chandrayaan
+  def navigate(commands)
+    curr_facing_direction = 'N'
+    # First position is for east and west (Increment in east and decrement in west)
+    # Second position is for north and south (Increment in north and decrement in south)
+    # Third position is for up and down (Increment in up and decrement in down)
+    curr_position = [0, 0, 0]
+    for command in commands
+      if command == 'f'
+        curr_position[1] += 1
+      end
+    end
+    curr_position
+  end
 end
